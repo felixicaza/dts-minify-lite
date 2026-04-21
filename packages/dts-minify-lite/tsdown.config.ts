46085d4837e281318fdbc59e1476b2e587f9f609
Felix Icaza <fx.joliett17@gmail.com>
2026-04-20 22:50:01 -0600

    chore(dts): add rolldownPluginDtsMinifyLite to tsdown configuration

--- packages/dts-minify-lite/tsdown.config.ts
+++ packages/dts-minify-lite/tsdown.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'tsdown'
+import { rolldownPluginDtsMinifyLite } from 'rolldown-plugin-dts-minify-lite'
 
 export default defineConfig({
   entry: 'src/index.ts',
   target: 'node24',
-  minify: true
+  minify: true,
+  plugins: [rolldownPluginDtsMinifyLite({ keepJsDocs: true })]
 })
