24f8ca5f183822d4e7c42d081f00d4e0247fcdcb
Felix Icaza <fx.joliett17@gmail.com>
2026-04-20 23:20:26 -0600

    fix(dts): add rolldown-plugin-dts-minify-lite as a devDependency

--- packages/dts-minify-lite/tsdown.config.ts
+++ packages/dts-minify-lite/tsdown.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'tsdown'
-import { rolldownPluginDtsMinifyLite } from '../rolldown-plugin-dts-minify-lite/src/index.ts'
+import { rolldownPluginDtsMinifyLite } from 'rolldown-plugin-dts-minify-lite'
 
 export default defineConfig({
   entry: 'src/index.ts',
